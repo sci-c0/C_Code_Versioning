23ad60bf74b1a87fe85d3e4fab56d19da773323f
electrogeek <Nishant-Mishra@users.noreply.github.com>
2016-12-06 12:33:02 +0530

    Add guards to avoid multiple includes.

--- version.h
+++ version.h
@@ -1,4 +1,21 @@
-#include <stdio.h>
+/*
+  @file  version.h
+  @brief Defines macros to create version number string
+
+  @license  MIT LICENSE
+  
+  @authors
+  Prakhar Nigam, https://github.com/prakharnigam
+  electrogeek, https://github.com/Nishant-Mishra
+  
+*/
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #define major      1
 #define minor      0
@@ -13,3 +30,9 @@
 #define dot        "."
 
 #define VERSION    (maj dot min dot pat)
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif /* __VERSION_H__ */
