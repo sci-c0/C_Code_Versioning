40e18ac7494b857933a4d6a6dbb18e9570a4c734
electrogeek <Nishant-Mishra@users.noreply.github.com>
2016-12-06 12:34:35 +0530

    Add comment header

--- test.c
+++ test.c
@@ -1,3 +1,16 @@
+/*
+  @file  test.c
+  
+  @brief Test code for the defined version
+  
+  @license  MIT LICENSE
+  
+  @authors
+  Prakhar Nigam, https://github.com/prakharnigam
+  electrogeek, https://github.com/Nishant-Mishra
+  
+*/
+
 #include <stdio.h>
 #include "version.h"
 
